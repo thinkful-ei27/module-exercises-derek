e9c909b8234b4de8ba015c69fcf114a24509a231
Derek Houck <derek@derekhouck.com>
2018-12-10 09:39:47 -0800

    Add create, update, and delete methods to store

--- scripts/store.js
+++ scripts/store.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* global cuid */
+/* global cuid, Item */
 
 // eslint-disable-next-line no-unused-vars
 const store = (function() {
@@ -12,13 +12,46 @@ const store = (function() {
   ];
   const hideCheckedItems = false;
   const searchTerm =  '';
-  const findById = function(name){
+  function findById(name) {
     store.items.find(item => item.name === name);
-  };
+  }
+  function addItem(name) {
+    try {
+      Item.validateName(name);
+      const newItem = Item.create(name);
+      this.items.push(newItem);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(`Cannot add item: ${err.message}`);
+    }
+  }
+  function findAndToggleChecked(id) {
+    const foundItem = this.findById(id);
+    foundItem.checked = !foundItem.checked;
+  }
+  function findAndUpdateName(id, newName) {
+    try {
+      Item.validateName(newName);
+      const item = this.findById(id);
+      item.name = newName;
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(`Cannot update name: ${err.message}`);
+    }
+  }
+  function findAndDelete(id) {
+    const index = this.items.findIndex(item => item.id === id);
+    this.items.splice(index, 1);
+  }
 
   return {
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
 }());
